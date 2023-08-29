a82c6f2c52f8f440d6827242f4ab5f743521dc91
Rishabh <rishabhtripathi587@gmail.com>
2023-08-29 11:11:53 +0530

    this is basic program for lerning

---
+++ Introduction to JS (Variables & Data Types/problem2.js
@@ -0,0 +1,8 @@
+var name="Rishabh";
+console.log(name);
+
+var name="shri. jagdish chandra tripathi";
+console.log(name);
+
+var name="jai laxmi tripathi";
+console.log(name);
---
+++ Introduction to JS (Variables & Data Types/problem3.js
@@ -0,0 +1,7 @@
+let name="Rishabh";
+let age= 19;
+
+console.log(name,age);
+
+console.log(typeof name);
+console.log(typeof age);
