f3950babb5a8c9f959feb7c39487e3cf19a6dd99
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2023-09-04 09:50:59 +0530

    added file 01

---
+++ Mathematical operators, Relational or Comparison Operators/01operation_on_number.js
@@ -0,0 +1,8 @@
+function operationOnNumbers(number) {
+    // Write code here
+    number=number*3;
+    number=number+7;
+    number=number-10;
+    console.log(number)
+}
+operationOnNumbers(3) //6
